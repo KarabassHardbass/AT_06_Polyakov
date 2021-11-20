78a04b5953f09c8d260ae077eb5bf45c86011de7
KarabasHardbass <kroko275@gmail.com>
2021-11-20 08:53:08 +0500

    Initial commit

---
+++ filter.py
@@ -0,0 +1,36 @@
+from PIL import Image
+import numpy as np
+
+print('Картинка')
+img = Image.open(input())
+print('Серый цвет')
+modifier = int(input())
+print('Размер')
+size = int(input())
+
+array = np.array(img)
+height = len(array)
+width = len(array[1])
+
+def find_grayscale(x, y):
+    scale = 0
+    for cur_y in range(y, y + size):
+        for cur_x in range(x, x + size):
+            if cur_y >= height or cur_x >= width:
+                print('Введён неправильный шаг')
+            scale += (array[cur_y][cur_x].sum()) // 3
+    return int(scale // (size * size))
+
+def fill_array(x, y, scale):
+    for cur_y in range(y, y + size):
+        for cur_x in range(x, x + size):
+            if cur_y >= height or cur_x >= width:
+                print('Введён неправильный шаг')
+            array[cur_y][cur_x] = np.repeat(int(scale // modifier) * modifier, 3)
+
+for y in range(0, height, size):
+    for x in range(0, width, size):
+        fill_array(x, y, find_grayscale(x, y))
+
+res = Image.fromarray(array)
+res.save('res.jpg')
